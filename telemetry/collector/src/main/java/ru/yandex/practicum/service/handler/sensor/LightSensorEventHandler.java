d9685cc35e3a3e2d58c793653d17563b16044759
Слава <korolslava0@gmail.com>
2025-11-21 13:37:55 +0700

    2-collector-grpc

--- telemetry/collector/src/main/java/ru/yandex/practicum/service/handler/sensor/LightSensorEventHandler.java
+++ telemetry/collector/src/main/java/ru/yandex/practicum/service/handler/sensor/LightSensorEventHandler.java
@@ -1,13 +1,16 @@
 package ru.yandex.practicum.service.handler.sensor;
 
+package ru.yandex.practicum.service.handler.sensor;
+
 import org.springframework.stereotype.Component;
+import ru.yandex.practicum.grpc.telemetry.event.LightSensorProto;
+import ru.yandex.practicum.grpc.telemetry.event.SensorEventProto;
 import ru.yandex.practicum.kafka.telemetry.event.LightSensorAvro;
-import ru.yandex.practicum.model.enums.SensorEventType;
-import ru.yandex.practicum.model.sensor.LightSensorEvent;
-import ru.yandex.practicum.model.sensor.SensorEvent;
 import ru.yandex.practicum.service.KafkaEventProducer;
 
-
+/**
+ * Обработчик события от датчика света
+ */
 @Component
 public class LightSensorEventHandler extends BaseSensorEventHandler<LightSensorAvro> {
     public LightSensorEventHandler(KafkaEventProducer producer) {
@@ -15,13 +18,13 @@ public class LightSensorEventHandler extends BaseSensorEventHandler<LightSensorA
     }
 
     @Override
-    public SensorEventType getMessageType() {
-        return SensorEventType.LIGHT_SENSOR_EVENT;
+    public SensorEventProto.PayloadCase getMessageType() {
+        return SensorEventProto.PayloadCase.LIGHT_SENSOR_EVENT;
     }
 
     @Override
-    protected LightSensorAvro mapToAvro(SensorEvent event) {
-        LightSensorEvent _event = (LightSensorEvent) event;
+    protected LightSensorAvro mapToAvro(SensorEventProto event) {
+        LightSensorProto _event = event.getLightSensorEvent();
 
         return LightSensorAvro.newBuilder()
                 .setLinkQuality(_event.getLinkQuality())
